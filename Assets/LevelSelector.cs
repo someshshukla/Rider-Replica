062696767ab77000af7077cf44b3f094fe09f284
Somesh Shukla <someshshukla@users.noreply.github.com>
2024-08-06 01:34:59 +0530

    Initial commit

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour {
+
+	public void SelectScene1()
+	{
+        SceneManager.LoadScene(2);
+    }
+
+    public void SelectScene2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SelectScene3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SelectScene4()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
